5cf9d64ba3f967ca9068c020b5ec5cf3e5211a14
EgeToksoz <31406681+EgeToksoz@users.noreply.github.com>
2023-03-04 01:47:52 +0300

    Add files via upload

---
+++ installer.bat
@@ -0,0 +1,126 @@
+@echo off
+Title Install Windows
+call powercfg /s 8c5e7fda-e8bf-4a96-9a85-a6e23a8c635c
+pushd %CD%
+cd /d %~dp0
+Echo list vol > "lv.txt"
+diskpart /s lv.txt
+del lv.txt
+set /P vt=Choose the Drive Letter for Ventoy Mounted ISO Partition: 
+set /p cleanuefi=Do you want to create partitions automatically or manually created them already (a)uto/(m)anual: 
+cls
+if %cleanuefi% == a goto auto
+if %cleanuefi% == m goto manual
+:auto
+Echo list disk > "cp.txt"
+diskpart /s cp.txt
+del cp.txt
+set /p disk=Choose the disk to create partitions on: 
+set /p clean=Do you want this disk to be cleaned or create from unallocated space (c)lean/(u)nallocated: 
+if %clean% == c goto conf
+if %clean% == u goto part
+
+:conf
+set /p sure=Are you sure about this action this will delete all data on this drive (y/n): 
+if %sure% == y goto clean
+if %sure% == n goto part
+
+:clean
+echo select disk %disk% > "cp.txt"
+echo clean >> "cp.txt"
+echo convert gpt >> "cp.txt"
+diskpart /s cp.txt
+del cp.txt
+cls
+:part
+set /p mini=Do you want the partitioning with windows standards or just efi and windows partition (f)ull/(m)ini: 
+if %mini% == f goto full
+if %mini% == m goto mini
+
+:full
+echo select disk %disk% > "cp.txt"
+echo create partition efi size=300 >> "cp.txt"
+echo format quick fs=fat32 >> "cp.txt"
+echo assign letter=S >> "cp.txt"
+echo create partition msr size=16 >> "cp.txt"
+echo create partition primary size=600 >> "cp.txt"
+echo format quick fs=ntfs >> "cp.txt"
+echo assign letter=R >> "cp.txt"
+echo set id="de94bba4-06d1-4d40-a16a-bfd50179d6ac" >> "cp.txt"
+echo gpt attributes=0x8000000000000001 >> "cp.txt"
+echo create partition primary >> "cp.txt"
+echo format quick fs=ntfs >> "cp.txt"
+echo assign letter=W >> "cp.txt"
+diskpart /s cp.txt
+del cp.txt
+goto cont
+:mini
+echo select disk %disk% > "cp.txt"
+echo create partition efi size=300 >> "cp.txt"
+echo format quick fs=fat32 >> "cp.txt"
+echo assign letter=S >> "cp.txt"
+echo create partition primary >> "cp.txt"
+echo format quick fs=ntfs >> "cp.txt"
+echo assign letter=W >> "cp.txt"
+diskpart /s cp.txt
+del cp.txt
+goto cont
+
+:manual
+set /p mnt=Do you want to mount a partition. Partition Letter Will be L: (y/n): 
+if %mnt% == y goto mount
+if %mnt% == n goto cont
+:mount
+echo list vol > "listvol.txt"
+diskpart /s listvol.txt
+del listvol.txt
+echo list disk > "mountdisk.txt"
+diskpart /s "mountdisk.txt"
+del mountdisk.txt
+set /p disk=Which disk would you like to mount?: 
+echo select disk %disk% > "listpar.txt"
+Echo list par >> "listpar.txt"
+diskpart /s listpar.txt
+del listpar.txt
+set /p partition=What partition would you like to mount?:
+echo select disk %disk% >> "mountpart.txt"
+echo select part %partition% >> "mountpart.txt"
+echo assign letter=L >> "mountpart.txt"
+diskpart /s mountpart.txt
+del mountpart.txt
+cls
+:cont
+cls
+set /P image=Do you want to install from default .wim or from a captured image (d)efault/(c)ustom: 
+if %image% == d goto installer
+if %image% == c goto custom
+cls
+:installer
+Echo list vol > "lv.txt"
+diskpart /s lv.txt
+del lv.txt
+set /P WIN=Choose the Drive Letter for Windows Partition(ex: W:): 
+cls
+dism /get-wiminfo /wimfile:"%vt%\sources\install.wim"
+set /P IND=Choose the Index Number from Selected File: 
+dism /Apply-Image /ImageFile:"%vt%\sources\install.wim" /Index:%IND% /ApplyDir:%WIN%
+cls
+goto efi
+:custom
+Echo list vol > "lv.txt"
+diskpart /s lv.txt
+del lv.txt
+set /P WIN=Choose the Drive Letter for Windows Partition(ex: W:): 
+set /P CAP=Type full path for where custom .wim file is: 
+cls
+dism /get-wiminfo /wimfile:%CAP%
+set /P IND=Choose the Index Number from Selected File: 
+dism /Apply-Image /ImageFile:%CAP% /Index:%IND% /ApplyDir:%WIN%\
+cls
+:efi
+Echo list vol > lv.txt"
+diskpart /s lv.txt
+del lv.txt
+set /P EFI=Choose the Driver Letter for EFI Partition(ex: S:): 
+bcdboot %WIN%\Windows /s %EFI% /f UEFI && bcdboot %WIN%\Windows /s %EFI% /f UEFI
+popd
---
+++ toolbox.bat
@@ -0,0 +1,76 @@
+@echo off
+Title Windows Toolbox
+pushd %CD%
+cd /d %~dp0
+echo list vol > "listvol.txt"
+diskpart /s listvol.txt
+del listvol.txt
+:sel
+echo Command - Description
+echo capture	Capture an Windows installation
+echo append	Append changes on saved Windows installation
+echo apply	Apply a Captured Windows image to a Partition
+echo esd	Convert ESD files to WIM files
+echo boot	    Add .ini drivers to boot.wim file 
+echo efi	    Mount the first partition of selected disk
+set /P sel= What do you want to do?
+if %sel% == capture goto capture
+if %sel% == append goto append
+if %sel% == apply goto apply
+if %sel% == esd goto esd
+if %sel% == boot goto boot
+if %sel% == efi goto efi
+if %sel% == e goto end
+
+:boot
+set /P WIM=Where is the boot.wim File: 
+set /P MNT=Where is the Mount Destination: 
+set /P DRV=Where is the Driver folder path: 
+
+dism /mount-image /imagefile:%WIM% /index:1 /mountdir:%MNT%
+dism /image:%MNT% /add-driver:%DRV% /recurse
+dism /unmount-image /mountdir:%MNT% /commit
+dism /mount-image /imagefile:%WIM% /index:2 /mountdir:%MNT%
+dism /image:%MNT% /add-driver:%DRV% /recurse
+dism /unmount-image /mountdir:%MNT% /commit
+
+:capture
+set /P CAP=Where will be the wim file go(ex: D:): 
+set /P WIN=Where is the windows drive located: 
+set /P NAM=What the name will be(This will be both the file name and image name): 
+dism /Capture-Image /ImageFile:"%CAP%\%NAM%.wim" /CaptureDir:%WIN%\ /Name:%NAM% /compress:fast /bootable
+goto end
+:append
+set /P CAP=Where will be the wim file go: 
+set /P WIN=Where is the windows drive located: 
+set /P NAM=What the name will be: 
+DISM /Append-Image /imagefile:"%CAP%\%NAM%.wim" /capturedir:%WIN%\ /Name:%NAM% /bootable
+goto end
+:apply
+set /P CAP=Where is the file located(full path): 
+set /P WIN=Where is the windows drive located:
+dism /get-wiminfo /wimfile:"%CAP%"
+goto end
+set /P IND=Choose the index number(if you appended it add +1 to times Append): 
+dism /Apply-Image /ImageFile:"%CAP%" /Index:%IND% /ApplyDir:%WIN%\
+:esd
+set /P ESD=Where is the ESD File (full path): 
+dism /get-wiminfo /wimfile:"%ESD%"
+set /P IND=choose version you want to export
+set /P EXP=Choose the Export Destination(full path): 
+dism /export-image /sourceimagefile:"%ESD%" /sourceindex:%IND% /destinationimagefile:%EXP% /compress:max /checkintegrity
+goto end
+:efi
+Echo list disk > "cp.txt"
+diskpart /s cp.txt
+del cp.txt
+set /p disk=Choose the Disk to mount the EFI:
+echo select disk %disk% > "cp.txt"
+echo select partition 1 >> "cp.txt"
+echo assign letter=S >> "cp.txt"
+diskpart /s cp.txt
+del cp.txt
+goto end
+
+:end
+exit
