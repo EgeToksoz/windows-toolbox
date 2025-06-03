bb9873c0d628f0a22ed1b87527adfa98db24e830
EgeToksoz <chiral.thaws-0q@icloud.com>
2025-06-03 17:28:20 +0100

    added driver install option

--- toolbox.bat
+++ toolbox.bat
@@ -13,6 +13,7 @@ echo apply	Apply a Captured Windows image to a Partition
 echo esd	Convert ESD files to WIM files
 echo boot	    Add .ini drivers to boot.wim file 
 echo efi	    Mount the first partition of selected disk
+echo mbp    Install Macbook Pro Drivers after installer completes
 set /P sel= What do you want to do?
 if %sel% == capture goto capture
 if %sel% == append goto append
@@ -20,6 +21,7 @@ if %sel% == apply goto apply
 if %sel% == esd goto esd
 if %sel% == boot goto boot
 if %sel% == efi goto efi
+if %sel% == mbp goto mbp
 if %sel% == e goto end
 
 :boot
@@ -33,7 +35,7 @@ dism /unmount-image /mountdir:%MNT% /commit
 dism /mount-image /imagefile:%WIM% /index:2 /mountdir:%MNT%
 dism /image:%MNT% /add-driver:%DRV% /recurse
 dism /unmount-image /mountdir:%MNT% /commit
-
+goto end
 :capture
 set /P CAP=Where will be the wim file go(ex: D:): 
 set /P WIN=Where is the windows drive located: 
@@ -56,7 +58,7 @@ dism /Apply-Image /ImageFile:"%CAP%" /Index:%IND% /ApplyDir:%WIN%\
 :esd
 set /P ESD=Where is the ESD File (full path): 
 dism /get-wiminfo /wimfile:"%ESD%"
-set /P IND=choose version you want to export
+set /P IND=choose version you want to export: 
 set /P EXP=Choose the Export Destination(full path): 
 dism /export-image /sourceimagefile:"%ESD%" /sourceindex:%IND% /destinationimagefile:%EXP% /compress:max /checkintegrity
 goto end
@@ -71,6 +73,10 @@ echo assign letter=S >> "cp.txt"
 diskpart /s cp.txt
 del cp.txt
 goto end
-
+:mbp
+set /P WIN=Where is the windows drive located: 
+set /P DRV=Where is the Driver folder path: 
+dism /image:%WIN% /add-driver:%DRV% /recurse
+goto end
 :end
 exit
